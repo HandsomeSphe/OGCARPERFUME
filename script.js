01ba24a5d90c1c563406347a392c1829d8c75da8
HandsomeSphe <siphelelehandsome@gmail.com>
2021-11-15 09:56:36 +0200

    added more javascript

--- script.js
+++ script.js
@@ -45,3 +45,88 @@ getintouch__button.addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+
+//Sending orders
+const order__type = document.querySelector("#name");
+const order__email = document.querySelector("#orderemail");
+const order = document.querySelector("#actualorder");
+const order__submit__button = document.querySelector(".popup__submit");
+
+function sendMail(e) {
+  e.preventDefault();
+  if (
+    order__type.value.length > 0 &&
+    order__email.value.length > 0 &&
+    order.value.length > 0
+  ) {
+    var tempParams = {
+      to_name: order__type.value,
+      from_name: order__email.value,
+      message: order.value,
+    };
+    console.log(
+      `${tempParams.from_name}, ${tempParams.to_name}, ${tempParams.message}`
+    );
+    emailjs
+      .send("service_aywm2sn", "template_o1r2iys", tempParams)
+      .then(function (res) {
+        console.log("sucess", res.status);
+      });
+    setTimeout(function () {
+      order__type.value = "";
+      order__email.value = "";
+      order.value = "";
+      order__submit__button.textContent = "succesful";
+      order__submit__button.style.backgroundColor = "green";
+      setTimeout(function () {
+        order__submit__button.textContent = "SUBMIT";
+        order__submit__button.style.backgroundColor = "#c7a92d";
+      }, 2000);
+    }, 1500);
+  } else {
+    order__type.focus();
+  }
+}
+order__submit__button.addEventListener("click", sendMail);
+
+//Sending Emails
+const user_name = document.querySelector(".name");
+const user__email = document.querySelector(".email");
+const user_message = document.querySelector("#mssg");
+const user__submit = document.querySelector(".submit__form");
+function sendContactMail(e) {
+  e.preventDefault();
+  if (
+    user_name.value.length > 0 &&
+    user__email.value.length > 0 &&
+    user_message.value.length > 0
+  ) {
+    var tempParams = {
+      to_name: user_name.value,
+      from_name: user__email.value,
+      message: user_message.value,
+    };
+    console.log(
+      `${tempParams.from_name}, ${tempParams.to_name}, ${tempParams.message}`
+    );
+    emailjs
+      .send("service_aywm2sn", "template_wfqhcq3", tempParams)
+      .then(function (res) {
+        console.log("sucess", res.status);
+      });
+    setTimeout(function () {
+      user_name.value = "";
+      user__email.value = "";
+      user_message.value = "";
+      user__submit.textContent = "succesful";
+      user__submit.style.backgroundColor = "green";
+      setTimeout(function () {
+        user__submit.textContent = "SUBMIT";
+        user__submit.style.backgroundColor = "#c7a92d";
+      }, 2000);
+    }, 1500);
+  } else {
+    user_name.focus();
+  }
+}
+user__submit.addEventListener("click", sendContactMail);
